d7ccf7bb7b75df4a424c98511ebb498f1c02e97f
VakhoNodadze <vakhtang.nodadze@geolab.edu.ge>
2019-03-03 22:31:07 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MoviesApp/precache-manifest.94871610e332e0e8953ff966a3891e48.js"
+  "/MoviesApp/movies/precache-manifest.39dfe88903cfe87b7a346d0a494427f5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MoviesApp/index.html", {
+workbox.routing.registerNavigationRoute("/MoviesApp/movies/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
