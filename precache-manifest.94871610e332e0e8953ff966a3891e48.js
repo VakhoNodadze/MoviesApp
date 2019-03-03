b309d124dd951a482b33b2e98c695e90355d7d74
VakhoNodadze <vakhtang.nodadze@geolab.edu.ge>
2019-03-03 22:40:42 +0400

    Updates

---
+++ precache-manifest.94871610e332e0e8953ff966a3891e48.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MoviesApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "2b7f0ab1e8a740254a51",
+    "url": "/MoviesApp/static/css/main.39374966.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MoviesApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a73c47871ec8b226cb21",
+    "url": "/MoviesApp/static/js/1.a73c4787.chunk.js"
+  },
+  {
+    "revision": "73b0a5866496ff89dc3e",
+    "url": "/MoviesApp/static/js/runtime~main.73b0a586.js"
+  },
+  {
+    "revision": "2b7f0ab1e8a740254a51",
+    "url": "/MoviesApp/static/js/main.2b7f0ab1.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MoviesApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MoviesApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MoviesApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a73c47871ec8b226cb21",
+    "url": "/MoviesApp/static/css/1.cfd33f77.chunk.css"
+  },
+  {
+    "revision": "fef2ee828fa868f34ca2d675482ce2cc",
+    "url": "/MoviesApp/index.html"
+  }
+];
