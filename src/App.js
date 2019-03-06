06a5aebe4c08cf52682b5498c57c5c2c216af66a
VakhoNodadze <vakhtang.nodadze@geolab.edu.ge>
2019-03-06 23:43:33 +0400

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/Movies";
-import NotFound from './components/NotFound';
+// import NotFound from './components/NotFound';
 import NavBar from './components/NavBar';
 import MovieForm from './components/MovieForm';
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Redirect, Switch, HashRouter } from "react-router-dom";
 import Favourites from './components/Favourites';
 // import Customers from './components/Customers';
 // import Rentals from './components/Rentals';
@@ -15,16 +15,18 @@ class App extends Component {
       <React.Fragment>
         <NavBar />
         <main className="container">
-          <Switch>
-            <Route path="/movies/:title" component={MovieForm} />
-            <Route path="/movies" component={Movies} />
-            {/* <Route path="/customers" component={Customers} />
+          <HashRouter>
+            <Switch>
+              <Route path="/movies/:title" component={MovieForm} />
+              <Route path="/movies" component={Movies} />
+              {/* <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} /> */}
-            <Route path="/favourites" component={Favourites} />
-            <Route path="/not-found" component={NotFound} />
-            <Redirect from="/" exact to="/movies" />
-            <Redirect to="/not-found" />
-          </Switch>
+              <Route path="/favourites" component={Favourites} />
+              {/* <Route path="/not-found" component={NotFound} /> */}
+              <Redirect from="/" exact to="/movies" />
+              <Redirect to="/not-found" />
+            </Switch>
+          </HashRouter>
         </main>
       </React.Fragment>
     );
